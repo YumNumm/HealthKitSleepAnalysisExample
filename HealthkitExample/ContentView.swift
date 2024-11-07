5a68c07ecb4f4024f69fac0eeac82345b871ce8d
YumNumm <onory0917@gmail.com>
2024-11-08 00:22:25 +0900

    Initial Commit

---
+++ HealthkitExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HealthkitExample
+//
+//  Created by 尾上 遼太朗 on 2024/11/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HealthkitExample/HealthkitExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthkitExampleApp.swift
+//  HealthkitExample
+//
+//  Created by 尾上 遼太朗 on 2024/11/08.
+//
+
+import SwiftUI
+
+@main
+struct HealthkitExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
